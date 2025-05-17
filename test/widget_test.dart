46fbfeb33ca9066ba21a59092366bf3b876d5631
DinushaRuw <162613989+DinushaRuw@users.noreply.github.com>
2025-05-17 11:35:40 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:my_project_name/main.dart';
 
@@ -5,7 +6,7 @@ void main() {
   testWidgets('App builds and shows a widget', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Check that at least one widget is shown (not empty)
+    // Check that at least one Scaffold widget is present
     expect(find.byType(Scaffold), findsWidgets);
   });
 }
